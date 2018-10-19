fe0d959db807afbf324e3603aa60de6d1c34c2ad
Ero Sobrino <ero.sobrino.dorado@gmail.com>
2018-10-19 12:24:46 +0200

    19/10/18

--- Bol4/Form1.Designer.cs
+++ Bol4/Form1.Designer.cs
@@ -28,13 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btSalir = new System.Windows.Forms.Button();
+            this.btColor = new System.Windows.Forms.Button();
+            this.tbR = new System.Windows.Forms.TextBox();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.tbG = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbImagen = new System.Windows.Forms.TextBox();
+            this.btImagen = new System.Windows.Forms.Button();
+            this.lblImagen = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btSalir
+            // 
+            this.btSalir.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btSalir.Location = new System.Drawing.Point(21, 23);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(75, 34);
+            this.btSalir.TabIndex = 0;
+            this.btSalir.Text = "Salir";
+            this.btSalir.UseVisualStyleBackColor = true;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            this.btSalir.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.btSalir.MouseLeave += new System.EventHandler(this.MouseLeave);
+            // 
+            // btColor
+            // 
+            this.btColor.Location = new System.Drawing.Point(594, 24);
+            this.btColor.Name = "btColor";
+            this.btColor.Size = new System.Drawing.Size(75, 33);
+            this.btColor.TabIndex = 1;
+            this.btColor.Text = "Color";
+            this.btColor.UseVisualStyleBackColor = true;
+            this.btColor.Click += new System.EventHandler(this.btColor_Click);
+            this.btColor.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.btColor.MouseLeave += new System.EventHandler(this.MouseLeave);
+            // 
+            // tbR
+            // 
+            this.tbR.Location = new System.Drawing.Point(155, 27);
+            this.tbR.Name = "tbR";
+            this.tbR.Size = new System.Drawing.Size(100, 22);
+            this.tbR.TabIndex = 2;
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(454, 26);
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(100, 22);
+            this.tbB.TabIndex = 3;
+            // 
+            // tbG
+            // 
+            this.tbG.Location = new System.Drawing.Point(304, 27);
+            this.tbG.Name = "tbG";
+            this.tbG.Size = new System.Drawing.Size(100, 22);
+            this.tbG.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(421, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(139, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ruta";
+            // 
+            // tbImagen
+            // 
+            this.tbImagen.Location = new System.Drawing.Point(198, 122);
+            this.tbImagen.Name = "tbImagen";
+            this.tbImagen.Size = new System.Drawing.Size(100, 22);
+            this.tbImagen.TabIndex = 9;
+            // 
+            // btImagen
+            // 
+            this.btImagen.Location = new System.Drawing.Point(320, 113);
+            this.btImagen.Name = "btImagen";
+            this.btImagen.Size = new System.Drawing.Size(75, 35);
+            this.btImagen.TabIndex = 10;
+            this.btImagen.Text = "Imagen";
+            this.btImagen.UseVisualStyleBackColor = true;
+            this.btImagen.Click += new System.EventHandler(this.btImagen_Click);
+            this.btImagen.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.btImagen.MouseLeave += new System.EventHandler(this.MouseLeave);
+            // 
+            // lblImagen
+            // 
+            this.lblImagen.AutoSize = true;
+            this.lblImagen.Location = new System.Drawing.Point(241, 189);
+            this.lblImagen.Name = "lblImagen";
+            this.lblImagen.Size = new System.Drawing.Size(0, 17);
+            this.lblImagen.TabIndex = 11;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(279, 228);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 17);
+            this.lblError.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btColor;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.CancelButton = this.btSalir;
+            this.ClientSize = new System.Drawing.Size(756, 407);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblImagen);
+            this.Controls.Add(this.btImagen);
+            this.Controls.Add(this.tbImagen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbG);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.tbR);
+            this.Controls.Add(this.btColor);
+            this.Controls.Add(this.btSalir);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btSalir;
+        private System.Windows.Forms.Button btColor;
+        private System.Windows.Forms.TextBox tbR;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.TextBox tbG;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbImagen;
+        private System.Windows.Forms.Button btImagen;
+        private System.Windows.Forms.Label lblImagen;
+        private System.Windows.Forms.Label lblError;
     }
 }
 
--- Bol4/Form1.cs
+++ Bol4/Form1.cs
@@ -12,9 +12,79 @@ namespace Bol4
 {
     public partial class Form1 : Form
     {
+        Color color;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Seguro que desea salir?", "Mi Aplicación",
+                   MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btColor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int rojo = Convert.ToInt32(tbR.Text);
+                int verde = Convert.ToInt32(tbG.Text);
+                int azul = Convert.ToInt32(tbB.Text);
+                if (rojo > 0 && verde > 0 && azul > 0 &&
+                    rojo < 255 && verde < 255 && azul < 255)
+                {
+                    this.BackColor = Color.FromArgb(rojo, verde, azul);
+                }
+            }
+            catch (OverflowException exc1)
+            {
+                lblError.Text = "Los valores son muy altos (0-255)";
+            }
+            catch (FormatException exc2)
+            {
+                lblError.Text = "Los valores deben ser numericos (0-255)";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximumSize = this.Size;
+            MinimumSize = this.Size;
+        }
+
+        private void MouseEnter(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            color = bt.BackColor;
+            bt.BackColor = Color.Blue;
+        }
+        private void MouseLeave(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            bt.BackColor = color;
+        }
+
+        private void btImagen_Click(object sender, EventArgs e)
+        {
+            //D:\Descargas\Hopstarter-Software-Mozilla-Firefox.ico
+            string ruta = tbImagen.Text;
+            if (ruta.Length != 0)
+            {
+                this.BackgroundImage = new Bitmap(ruta);
+            }
+            else
+            {
+                this.BackgroundImage = null;
+            }
         }
     }
 }
